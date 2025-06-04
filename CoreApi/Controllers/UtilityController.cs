7e2ee47be126070a9e6412ce0c340da7734fe498
wjkonger <wjkonger@gmail.com>
2025-06-04 15:28:45 -0400

    Export all settings value

--- CoreApi/Controllers/UtilityController.cs
+++ CoreApi/Controllers/UtilityController.cs
@@ -9,11 +9,13 @@ namespace CoreApi.Controllers
     [ApiController]
     public class UtilityController : ControllerBase
     {
-       private readonly IAmazonSecretsManager _secretsManager;
+        private readonly IAmazonSecretsManager _secretsManager;
+        private readonly IConfiguration _configuration;
 
-        public UtilityController(IAmazonSecretsManager secretsManager)
+        public UtilityController(IAmazonSecretsManager secretsManager, IConfiguration configuration)
         {
             _secretsManager = secretsManager;
+            _configuration = configuration;
         }
 
         [HttpGet("secret/{name}")]
@@ -24,7 +26,21 @@ namespace CoreApi.Controllers
             var secret = await _secretsService.GetSecretValueAsync(name);
             return Ok(secret);
         }
-        
-        
+       
+
+
+
+        [HttpGet("settings")]
+        public IActionResult GetAllSettings()
+        {
+            var configData = new Dictionary<string, string>();
+
+            foreach (var kvp in _configuration.AsEnumerable())
+            {
+                configData[kvp.Key] = kvp.Value;
+            }
+
+            return Ok(configData);
+        }
     }
 }
